bcd9c832f4444275442a6891f28dd3e4e1a7eb6c
Valeriy Nehovora <valeriynegovora@gmail.com>
2022-01-14 05:45:20 +0000

    Implementing Admin Dashboard-Count

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,11 @@
 class Admin::DashboardController < ApplicationController
+  
+  http_basic_authenticate_with name: ENV['USERNAME'], password: ENV['PASSWORD']
 
   def show
+    @total_items_quantaty = Product.all.count
+    @number_categories = Category.all.count
   end
 end
+
+
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,6 +3,7 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all.order(created_at: :desc)
   end
+  puts @products
 
   def show
     @product = Product.find params[:id]

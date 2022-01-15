df9e5e8b12e03bfdc26f472ca108217995db6a94
Valeriy Nehovora <valeriynegovora@gmail.com>
2022-01-15 15:56:35 +0000

    implement feature delleting only empty category

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -10,15 +10,20 @@ class Admin::CategoriesController < ApplicationController
     @category = Category.new
   end
 
+ 
+
   def create
     @category = Category.new(category_params)
 
-    if @category.save
+    
+    if  @category.save
       redirect_to [:admin, :categories], notice: 'Category created!'
     else
       render :new
     end
   end
+
+
 
 def destroy
   @category = Category.find params[:id]
